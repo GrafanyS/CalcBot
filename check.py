bca3f560bf044807828ed80365e81c34ba94930d
GrafanyS <admin@valet.of.by>
2022-10-06 23:32:11 +0300

    Start

--- check.py
+++ check.py
@@ -1,14 +1,22 @@
-def get_ints():
-    """
-    Проверка на ввод числа
-    """
-    while True:
-        try:
-            num = int(input('value = '))
-            return num
-        except ValueError:
-            print('Ошибка. Ожидалось вещественное число.')
+# def get_ints():
+#     """
+#     Проверка на ввод числа
+#     """
+#     while True:
+#         try:
+#             num = int(input('value = '))
+#             return num
+#         except ValueError:
+#             print('Ошибка. Ожидалось вещественное число.')
+#
+#
+# def test_division(x, y):
+#     """Проверка деление на 0 """
 
 
-def test_division(x, y):
-    """Проверка деление на 0 """
+def check_realnumber(update, num):
+    return None
+
+
+def check_operation(update, oper):
+    return None
